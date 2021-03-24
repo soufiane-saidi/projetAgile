c09c9e9acca8d2a104c13f10b0981088168eb500
soufiane <e22012768@etudiant.univ-brest.fr>
2021-03-24 16:08:43 +0100

    24mars_commit

--- src/main/java/com/projetAgile/ubo/Controllers/EnseignantController.java
+++ src/main/java/com/projetAgile/ubo/Controllers/EnseignantController.java
@@ -4,11 +4,12 @@ import com.projetAgile.ubo.DTO.EnseignantsDTO;
 import com.projetAgile.ubo.Entities.Enseignant;
 import com.projetAgile.ubo.Services.EnseignantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
 @RestController
 public class EnseignantController {
 
